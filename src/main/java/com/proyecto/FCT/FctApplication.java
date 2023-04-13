66e4e908762e075aed6725d9790b1b4f0016109a
HéctorVara <hector.vara@nunegal.com>
2023-04-13 15:45:22 +0200

    Esqueleto del proyecto Spring Boot con las dependencias principales añadidas

---
+++ src/main/java/com/proyecto/FCT/FctApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.FCT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FctApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FctApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyecto/FCT/FctApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.FCT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FctApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

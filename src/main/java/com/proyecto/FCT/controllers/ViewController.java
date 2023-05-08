fb58176cb01ec0f26525e1a75b9f156e114ccc96
HéctorVara <hector.vara@nunegal.com>
2023-05-08 12:20:07 +0200

    Se crea un endpoint para eliminar todos los archivos de la base de datos y se añade la dependencia de Thyme Leaf en el pom.xml

---
+++ src/main/java/com/proyecto/FCT/controllers/ViewController.java
@@ -0,0 +1,16 @@
+package com.proyecto.FCT.controllers;
+
+import com.proyecto.FCT.repositories.DocumentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+public class ViewController {
+    @Autowired
+    DocumentRepository documentRepository;
+
+    @GetMapping("/")
+    public String showHomePage(){
+        return "index";
+    }
+}
